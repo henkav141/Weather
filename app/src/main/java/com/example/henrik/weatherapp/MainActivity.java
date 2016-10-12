4cdfb374430d40db98d406d7a63f608a19900379
Henrik Karlsson <henrik.karlsson.88@live.se>
2016-10-12 23:41:14 +0200

    First commit
    First commit

---
+++ app/src/main/java/com/example/henrik/weatherapp/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.henrik.weatherapp;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.henrik.weatherapp.Weather.JSONWeatherParser;
+import com.example.henrik.weatherapp.Weather.Weather;
+import com.example.henrik.weatherapp.Weather.WeatherHttpClient;
+
+import org.json.JSONException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView city;
+    private TextView temp;
+    private TextView desc;
+    private ImageView imgView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String cit = "London,UK";
+        city = (TextView) findViewById(R.id.textCity);
+        temp = (TextView) findViewById(R.id.textTemp);
+        desc = (TextView) findViewById(R.id.textDesc);
+        imgView = (ImageView) findViewById(R.id.imgView);
+
+        JSONWeatherTask task = new JSONWeatherTask();
+        task.execute(new String[]{cit});
+    }
+
+    private class JSONWeatherTask extends AsyncTask<String, Void, Weather> {
+
+        @Override
+        protected Weather doInBackground(String... params) {
+            Weather weather = new Weather();
+            String data = ( (new WeatherHttpClient()).getWeatherData(params[0]));
+
+            try {
+                weather = JSONWeatherParser.getWeather(data);
+
+                // Let's retrieve the icon
+                weather.iconData = ( (new WeatherHttpClient()).getImage(weather.currentCondition.getIcon()));
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return weather;
+
+        }
+
+
+
+
+        @Override
+        protected void onPostExecute(Weather weather) {
+            super.onPostExecute(weather);
+
+            if (weather.iconData != null && weather.iconData.length > 0) {
+                Bitmap img = BitmapFactory.decodeByteArray(weather.iconData, 0, weather.iconData.length);
+                imgView.setImageBitmap(img);
+            }
+
+            city.setText(weather.location.getCity() + "," + weather.location.getCountry());
+            desc.setText(weather.currentCondition.getCondition() + "(" + weather.currentCondition.getDescr() + ")");
+            temp.setText("" + Math.round((weather.temperature.getTemp() - 273.15)) + "�C");
+           /* hum.setText("" + weather.currentCondition.getHumidity() + "%");
+            press.setText("" + weather.currentCondition.getPressure() + " hPa");
+            windSpeed.setText("" + weather.wind.getSpeed() + " mps");
+            windDeg.setText("" + weather.wind.getDeg() + "�");*/
+
+        }
+    }
+}
